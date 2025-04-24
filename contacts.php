49e1e06fe180773cbdfe79df58ba1f8336982c5c
Fatima Bian Arnigo <arnigo.fatimabian26@gmail.com>
2025-04-24 15:31:07 +0800

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -3,14 +3,14 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>EcoTrack - Contact</title>
+  <title>EcoTrack - Contacts</title>
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" />
 
   <style>
     body {
-      background-image: url('truck.png');
+      background-image: url('truck1.png');
       background-size: cover;
       background-repeat: no-repeat;
       background-attachment: fixed;
