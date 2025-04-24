4d056a8d6f0bb24c8e0aeea9ce507bee5251b061
Kram029 <mjsaniel32@gmail.com>
2025-04-24 17:22:30 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>EcoTrack Navbar</title>
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" />
+<style>
+  .footer {
+      background-color: #f5f5f5;
+      padding: 20px;
+      text-align: center;
+      font-size: 20px;
+    }
+
+    .footer a {
+      color: #0000ee;
+      text-decoration: none;
+      font-weight: bold;
+    }
+
+    .footer-links {
+      margin-top: 10px;
+    }
+    .black-line {
+      height: 2px;
+      background-color: black;
+    }
+
+    
+  </style>
+  <div class="black-line"></div>
+  <footer class="footer">
+    <div class="footer-links">
+      <a href="privacy-statement.html">Privacy Statement</a> |
+      <a href="terms-and-conditions.html">Terms and Condition</a> |
+      <a href="privacy-policy.html">Privacy Policy</a>
+    </div>
+    <div class="copyright">
+      @2025 EcoTrack. All Rights Reserved.
+    </div>
+  </footer>
+
+</body>
+</html>
