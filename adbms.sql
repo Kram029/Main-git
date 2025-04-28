0bef4a226d225e245c63f8d7a2dcacbedde1d263
Fatima Bian Arnigo <arnigo.fatimabian26@gmail.com>
2025-04-28 15:02:04 +0800

    Update adbms.sql

--- adbms.sql
+++ adbms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 24, 2025 at 09:52 AM
+-- Generation Time: Apr 28, 2025 at 08:57 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,17 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `adbms`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `images`
---
-
-CREATE TABLE `images` (
-  `id` int(11) NOT NULL,
-  `file` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -65,17 +54,12 @@ INSERT INTO `users` (`id`, `first_name`, `middle_name`, `last_name`, `suffix`, `
 (12, 'mj', 'dsds', 'fdgfdgd', 'sddsd', 'mj@gmail.com', '09746573458', 'lipa', 'Region', 'Province', 'City', 'Barangay', 'mjsaniel', '$2y$10$tUGYjZVeCYEL7hgEgUyRceb6XjhLPI670Lp4QaCErTKGQDYg3u8oO', '2025-04-18 08:42:58'),
 (14, 'MarkJoseph', 'Puso', 'Saniel', 'P', 'mjsaniel32@gmail.com', '09999999999', 'lipa', 'Region', 'Province', 'City', 'Barangay', 'Kram_29', '$2y$10$YwgeaF01h9GfeDwwfblqTOEgMwSi9WuwDahVfR7Ed0XfswHUbejh2', '2025-04-23 11:43:43'),
 (17, 'MarkJoseph', 'Puso', 'Saniel', 'P', 'mj01@gmail.com', '09999999999', 'lipa', 'Region', 'Province', 'City', 'Barangay', 'loading_29', '$2y$10$2EEmvozcC4873NwQUdrtyuTNOVvU7LQimN/NJTwFozLUkcaXGJL8e', '2025-04-23 11:49:22'),
-(18, 'John', 'dsds', 'Doe', '', 'mj02@gmail.com', '09999999999', '', 'Region', 'Province', 'City', 'Barangay', 'Kram_99', '$2y$10$ZC9DGy0pMiZuTfkAM7Pc3eBtIliWDOqeYnmoaGAcZ2Z2DEooO35nG', '2025-04-24 07:46:34');
+(18, 'John', 'dsds', 'Doe', '', 'mj02@gmail.com', '09999999999', '', 'Region', 'Province', 'City', 'Barangay', 'Kram_99', NULL, '2025-04-24 07:46:34'),
+(20, 'Fatima', 'Ramirez', 'Arnigo', 'dqfeq', 'arnigofatimabian@gmail.com', '09935048514', 'Purok', 'Region', 'Province', 'City', 'Barangay', 'fatimabian', 'Fatima#26', '2025-04-24 09:48:00');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `images`
---
-ALTER TABLE `images`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -90,16 +74,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `images`
---
-ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
